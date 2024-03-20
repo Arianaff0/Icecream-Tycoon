835496e7066e1fec829dc5d482153a1ff97ea7cc
Matthew Molloy <mmolloy4@uwo.ca>
2024-03-20 15:01:18 -0400

    No new changes in GameLauncher yet. Will be soon

--- GameLauncher.java
+++ GameLauncher.java
@@ -1,11 +1,10 @@
-package lukas;
 
 
 /**
  * @author Lukas Bozinov
  */
 public class GameLauncher {
-
+	
 	/**
 	 * This method is the main method of the program. 
 	 * This is what needs to be compiled and run for the program to execute.
--- TitleScreen.java
+++ TitleScreen.java
@@ -1,4 +1,4 @@
-package lukas;
+
 
 import java.awt.Color;
 import java.awt.Font;
